8f73cf1d9e019765a084bb5439697e8b415f595b
CarlosETB <carloseduardotonholi@gmail.com>
2020-07-17 16:51:12 -0300

    fix(): reset listen

--- src/server.ts
+++ src/server.ts
@@ -11,13 +11,19 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-const currentPath = process.env.PWD || __dirname;
-app.use("/uploads", express.static(path.resolve(currentPath, "assets")));
+// const currentPath = process.env.PWD || __dirname;
+// app.use("/uploads", express.static(path.resolve(currentPath, "assets")));
+
+// app.use(errors());
+
+// const PORT = process.env.PORT || 3333;
+// app.listen(PORT, () => {
+//   console.log(`sesese ${currentPath}`);
+//   console.log(`Server started on port ${PORT}`);
+// });
+
+app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 
 app.use(errors());
 
-const PORT = process.env.PORT || 3333;
-app.listen(PORT, () => {
-  console.log(`sesese ${currentPath}`);
-  console.log(`Server started on port ${PORT}`);
-});
+app.listen(3333);
